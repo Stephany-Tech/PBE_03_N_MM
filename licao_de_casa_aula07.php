0039ac020bb21df0997c7eee6b0d9701a38d5090
Stephany-Teste <stephany26bs@uni9.edu.br>
2025-09-24 20:17:42 -0300

    lição de casa 07

---
+++ licao_de_casa_aula07.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lição_de_Casa_aula07</title>
+</head>
+<body>
+    <h1>Operadores - Comparação, Lógico, Ternário</h1>
+    
+</body>
+</html>
