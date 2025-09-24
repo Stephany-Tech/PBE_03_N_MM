1dc18a5dd1b8b89e30c8b898363da1955b2ce8e6
Stephany-Teste <stephany26bs@uni9.edu.br>
2025-09-24 20:33:53 -0300

    comparação

--- licao_de_casa_aula07.php
+++ licao_de_casa_aula07.php
@@ -7,6 +7,22 @@
 </head>
 <body>
     <h1>Operadores - Comparação, Lógico, Ternário</h1>
-    
 </body>
 </html>
+<?php
+$a=10;
+$b="10";
+echo "Igualdade = ".($a==$b)."<br>"; // == igual 
+echo "Idêntico = ".($a===$b)."<br>"; // ==idêntico
+echo "Não igual = ".($a!=$b)."<br>"; //não igual
+echo "Não Idêntico = ".($a!==$b)."<br>"; //!== não idêntico 
+?>
+<?php
+$a=10;
+$b=10;
+$c=11;
+echo "Menor: ".($a<$b)."<br>"; // < menor
+echo "Menor ou igual: ".($a<=$b)."<br>"; // <= menor ou igual 
+echo "Maior: ".($c>$b)."<br>"; //> maior
+echo "Maior ou igual: ".($c>=$b)."<br>"; // >= maior ou igual 
+?>
