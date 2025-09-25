0c599c6ddbc0653bf7d5d39350d50027385dd7f3
Stephany-Teste <stephany26bs@uni9.edu.br>
2025-09-24 23:00:21 -0300

    alteração

--- licao_de_casa_aula07.php
+++ licao_de_casa_aula07.php
@@ -7,103 +7,100 @@
 </head>
 <body>
     <h1>Operadores - Comparação, Lógico, Ternário</h1>
-</body>
-</html>
 
+    <h2>Operador de Comparação</h2>
+    <?php
+    $a=10;
+    $b="10";
+    echo "Igualdade = ".($a==$b)."<br>";
+    echo "Idêntico = ".($a===$b)."<br>";
+    echo "Não igual = ".($a!=$b)."<br>";
+    echo "Não Idêntico = ".($a!==$b)."<br>";
+    ?>
+    <?php
+    $a=10;
+    $b=10;
+    $c=11;
+    echo "Menor: ".($a<$b)."<br>";
+    echo "Menor ou igual: ".($a<=$b)."<br>";
+    echo "Maior: ".($c>$b)."<br>";
+    echo "Maior ou igual: ".($c>=$b)."<br>";
+    ?>
 
-<h2>Operador de Comparação</h2>
-<?php
-$a=10;
-$b="10";
-echo "Igualdade = ".($a==$b)."<br>"; // == igual - compara apenas os valores das variavies, iguinorando o tipo de dado
-echo "Idêntico = ".($a===$b)."<br>"; // ==idêntico - compara valores e tipos de dados, e verifica se os tipos de dados são os mesmos
-echo "Não igual = ".($a!=$b)."<br>"; //não igual - compara se os valores das variaveis são diferentes 
-echo "Não Idêntico = ".($a!==$b)."<br>"; //!== não idêntico - compara se os valores são diferentes ou se os tipos são diferentes
-?>
-<?php
-$a=10;
-$b=10;
-$c=11;
-echo "Menor: ".($a<$b)."<br>"; // < menor
-echo "Menor ou igual: ".($a<=$b)."<br>"; // <= menor ou igual 
-echo "Maior: ".($c>$b)."<br>"; //> maior
-echo "Maior ou igual: ".($c>=$b)."<br>"; // >= maior ou igual 
-?>
+    <h2>Operador lógico</h2>
+    <h3> Não - not - !</h3>
+    <?php
+    $a=50;
+    $b=120;
+    $c= ($a<=$b);
+    $d= !($a<=$b);
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    ?>
 
+    <h3> E - and - &&</h3>
+    <?php
+    $a=50;
+    $b=120;
+    $c=200;
+    $d=(($a<=$b) and ($a<=$c));
+    $e=(($a<=$b) && ($a>=$c));
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    ?>
 
-<h2>Operador lógico</h2>
+    <h3> Ou - or - ||</h3>
+    <?php
+    $a=50;
+    $b=120;
+    $c=200;
+    $d=(($a>=$b) or ($a>=$c));
+    $e=(($a>=$b) || ($a<=$c));
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    ?>
 
-<h3> Não - not - !</h3>
-<?php
-$a=50;
-$b=120;
-$c= ($a<=$b);
-$d= !($a<=$b);
-var_dump($c);
-echo "<br>";
-var_dump($d);
-?>
+    <h3>Xor</h3>
+    <?php
+    $a=50;
+    $b=120;
+    $c=200;
+    $d=(($a<=$b) xor ($c<=$a));
+    $e=(($a>=$b) xor ($a>=$c));
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    ?>
 
-<h3> E - and - &&</h3>
-<?php
-$a=50;
-$b=120;
-$c=200;
-$d=(($a<=$b) and ($a<=$c));
-$e=(($a<=$b) &&  ($a>=$c));
-var_dump($d);
-echo "<br>";
-var_dump($e);
-?>
+    <h2>Operador Ternário</h2> 
+    <?php
+    $a=50;
+    $b=120;
+    $c=200;
+    $d=($a<=$b) ? "Verdadeiro" : "Falso";
+    $e=($a>=$c) ? "Verdadeiro" : "Falso";
+    echo "d = $d<br>e = $e";
+    ?>
+    <br>
 
-<h3> Ou - or - ||</h3>
-<?php
-$a=50;
-$b=120;
-$c=200;
-$d=(($a>=$b) or ($a>=$c));
-$e=(($a>=$b) || ($a<=$c));
-var_dump($d);
-echo "<br>";
-var_dump($e);
-?>
+    ---
 
-<h3>Xor</h3>
-<?php
-$a=50;
-$b=120;
-$c=200;
-$d=(($a<=$b) xor ($c<=$a));
-$e=(($a>=$b) xor ($a>=$c));
-var_dump($d);
-echo "<br>";
-var_dump($e);
-?>
+    <h2>Formulário de idade e resultado com Operador de Comparação, Lógico e Ternário</h2>
+    <form method="post" action=""> 
+        Idade: <input type="number" name="idade" required><br>
+        <button type="submit">Classificar</button>
+    </form>
 
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Pega o valor da idade do formulário e converte para número
+        $idade = (int)$_POST["idade"];
+        $mensagem_ternario = "";
 
-<h2>Operador Ternário</h2> 
-<?php
-$a=50;
-$b=120;
-$c=200;
-$d=($a<=$b) ? "Verdadeiro" : "Falso";
-$e=($a>=$c) ? "Verdadeiro" : "Falso";
-echo "d = $d<br>e = $e";
-?>
-<br>
-
-<h2>Formulário de idade e resultado com Operador de Comparação, Lógico e Ternário></h2>
-    <form method='post' action""> 
-        Idade: <input type="number" name="Idade: " required><br>
-        <button type="submit">Classificar<button>
-</form>
-
-<?phpif ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Pega o valor da idade do formuário e converte para número
-    $idade = (int)$_POST["idade"];
-    $mensagem_ternario = "";
-
-     // Usando IF e operadores de comparação e lógicos para a classificação principal
+        // Usando IF e operadores de comparação e lógicos para a classificação principal
         if ($idade <= 12) {
             $mensagem_principal = "Resultado: Criança.";
         } elseif ($idade > 12 && $idade < 18) {
@@ -124,5 +121,3 @@ echo "d = $d<br>e = $e";
     ?>
 </body>
 </html>
-}
-
