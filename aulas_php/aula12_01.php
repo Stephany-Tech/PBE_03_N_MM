baa5192e6a413761fc599f574df0c98a53f9d6a7
Stephany-Teste <stephany26bs@uni9.edu.br>
2025-11-06 17:30:05 -0300

    aula 12

---
+++ aulas_php/aula12_01.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 12_ 01 - 06/11/2025</title>
+</head>
+<body>
+<?php
+ echo "<h1>Variáveis do Tipo Array</h1>"
+?>
+    
+</body>
+</html>
